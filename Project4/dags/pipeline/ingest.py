ae27cb7b0091875600423eeffca7b27046b7744c
kirkekrusell <kirke.krusell@elering.ee>
2026-05-16 20:28:13 +0300

    update

--- Project4/dags/pipeline/ingest.py
+++ Project4/dags/pipeline/ingest.py
@@ -1,10 +1,11 @@
-
 from io import BytesIO
 from pipeline.utils import get_db, get_s3, MINIO_BUCKET, sha256_bytes
 
 def run_ingest(limit, **context):
+    # Kõik masinõppe impordid on rangelt ainult siin funktsiooni sees!
     from datasets import load_dataset
     from PIL import Image
+
     run_id = context["ti"].xcom_pull(key="run_id")
     s3 = get_s3()
     conn = get_db()
@@ -21,14 +22,14 @@ def run_ingest(limit, **context):
         for row in chosen:
             sid = int(row["screenId"])
 
-            # PNG salvestamine MinIO-sse
+            # PNG faili salvestamine MinIO-sse
             png_buf = BytesIO()
             row["image"].save(png_buf, format="PNG")
             png_bytes = png_buf.getvalue()
             png_key = f"screens/{sid}.png"
             s3.put_object(Bucket=MINIO_BUCKET, Key=png_key, Body=png_bytes)
 
-            # JSON salvestamine MinIO-sse
+            # JSON faili salvestamine MinIO-sse
             json_bytes = row["view_hierarchy"].encode("utf-8")
             json_key = f"screens/{sid}.json"
             s3.put_object(Bucket=MINIO_BUCKET, Key=json_key, Body=json_bytes)
@@ -40,14 +41,7 @@ def run_ingest(limit, **context):
                 INSERT INTO screens_metadata
                 (screen_id, app_package, category, png_path, hierarchy_json_path, run_id, source_fingerprint)
                 VALUES (%s, %s, %s, %s, %s, %s, %s)
-                ON CONFLICT (screen_id) 
-                DO UPDATE SET 
-                    app_package = EXCLUDED.app_package,
-                    category = EXCLUDED.category,
-                    png_path = EXCLUDED.png_path,
-                    hierarchy_json_path = EXCLUDED.hierarchy_json_path,
-                    run_id = EXCLUDED.run_id,
-                    source_fingerprint = EXCLUDED.source_fingerprint
+                ON CONFLICT (screen_id) DO NOTHING
                 """,
-                (sid, row.get("package_name", "unknown"), row.get("category", "unknown"), png_key, json_key, run_id, fingerprint),
+                (sid, row.get("package_name", "unknown"), row.get("category", "unknown"), png_key, json_key, run_id, fingerprint)
             )
